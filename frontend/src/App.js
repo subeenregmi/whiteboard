878d5f4212eb568bd5187085c897f5a460d83e01
Gagan G <gnawalign@gmail.com>
2024-12-17 16:38:29 +0000

    basic drawing functionality from canvas component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
+function Canvas() {
+    useEffect (() => {
+        const canvas = document.getElementById("whiteboard");
+        const context = canvas.getContext("2d");
+        let xcor = 0;
+        let ycor = 0;
+        const penWidth = 3;
+        let painting = false;
+
+        function updatePos(event) {
+            xcor = event.clientX;
+            ycor = event.clientY;
+        }
+
+        function setPainting(event) {
+            updatePos(event)
+            painting = true
+        }
+
+        function unsetPainting() {
+            painting = false
+        }
+
+        function draw(event) {
+            if (painting) {
+                context.lineWidth = penWidth;
+                context.moveTo(xcor, ycor)
+                updatePos(event);
+                context.lineTo(xcor, ycor);
+                context.stroke()
+            }
+
+        }
+
+        document.addEventListener("mousedown", setPainting);
+        document.addEventListener("mouseup", unsetPainting);
+        document.addEventListener("mousemove", draw);
+
+    });
+
+    return (
+        // canvas width and height is the entire window for now, will consider
+        // tool bar later
+        <canvas id="whiteboard" width={window.innerWidth} height={window.innerHeight}>
+        </canvas>
+    );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Canvas />
     </div>
   );
 }
