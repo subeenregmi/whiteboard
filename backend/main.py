e18e929033b002ab0cd7e3aed840a09cb6994d7c
Gagan G <gnawalign@gmail.com>
2025-07-04 17:27:20 +0100

    feat!(backend): updated backend to make use of fastapi instead of flask

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello world!</p:"
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
