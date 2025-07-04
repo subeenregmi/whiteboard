49cc22b52d2698b60ca384ebfbae7eaf448e223f
Gagan G <gnawalign@gmail.com>
2025-07-04 18:43:25 +0100

    feat: initial setup for websocket handling on the backend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from typing import *
+from ws import ConnectionManager
 
 app = FastAPI()
 
+# mapping between the board_id and the manager for that board
+managers: Dict[int, ConnectionManager] = {}
+
+# could use query parameters to implement password to join boards
+@app.websocket("/ws/{board_id}")
+async def websocket_endpoint(websocket: WebSocket, board_id: int):
+    # create new board if doesnt already exist
+    if board_id not in managers:
+        managers[board_id] = ConnectionManager()
+    
+    manager = managers[board_id]
+    await manager.conenct(websocket)
+    
+    try:
+        while True:
+            data = await websocket.receive_json()
+            await manager.broadcast_board_data()
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
 
 @app.get("/")
 async def root():
