86b873e35e59a919b5af141bbc1828ebc73d70a4
Subeen Regmi <subeenregmi123@gmail.com>
2025-07-10 22:53:35 +0100

    feat: added a pydantic model for a single user stroke

---
+++ backend/models/stroke.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+from pydantic_extra_types.color import Color
+
+
+class Stroke(BaseModel):
+    id: int
+    uname: str
+    timestamp: datetime
+    coordinates: list[tuple[int, int]]
+    color: Color
